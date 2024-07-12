2d304fb0348c4ced6292d1e1ee9d00be97bdfa5f
sentry456123 <sentry456123@gmail.com>
2024-07-12 11:32:17 +0900

    second commit

--- LinuxDebloater.sh
+++ LinuxDebloater.sh
@@ -1,2 +1,5 @@
 echo "Debloating Linux operating system..."
 echo "Done!"
+echo "0 pre-installed software removed"
+echo "0 telemetry functions disabled"
+echo "0 unnecessary scheduled tasks disabled"
